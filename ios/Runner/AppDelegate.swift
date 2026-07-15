b2b3cc0478ec212338a6e99c26149991fd31bca0
Patricio Yunga <guaman1579@gmail.com>
2026-07-15 15:19:17 -0500

    configuracion de ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,25 @@
 import Flutter
 import UIKit
+import GoogleMaps
+import flutter_local_notifications
+import FirebaseCore
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate 
+{
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+    GMSServices.provideAPIKey("AIzaSyD2XmZoDR1G3_vstDE4wDUZQ9KyfPjxE")
+    GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
