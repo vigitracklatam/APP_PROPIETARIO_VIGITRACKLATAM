a7cf703573ae16f2cc214d7b37be2feb08ddb461
VIGITRACKLATAM CIA.LTDA <vigitracklatam@MacBook-Pro-de-VIGITRACKLATAM.local>
2026-07-15 17:37:07 -0500

    cambio en la clave para el mapa

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,25 +5,31 @@ import flutter_local_notifications
 import FirebaseCore
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate 
-{
+@objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     FirebaseApp.configure()
+
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
       GeneratedPluginRegistrant.register(with: registry)
     }
-    GMSServices.provideAPIKey("AIzaSyD2XmZoDR1G3_vstDE4wDUZQ9KyfPjxE")
+
+    GMSServices.provideAPIKey("AIzaSyCJ8BazUPx9gsywvwpyTXMTXo2P0BGF_WM")
+    //GMSServices.setMetalRendererEnabled(true)
+
     GeneratedPluginRegistrant.register(with: self)
+
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
     }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 }
